8e90ac69645ea26f8c56fce8441b98be0689e9e2
zano8 <30663434+zano8@users.noreply.github.com>
2017-08-09 22:33:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,76 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var blogs = {
+    'blog-one': {
+        title: 'my first blog',
+        heading: 'my favourite fictional characters :',
+        list: `   <ol> <li>newt (tmr)</li>
+                    <li>minho (tmr)</li>
+                    <li>gale (thg)</li>
+                    <li>cinna (thg)</li>
+                    <li>hermoine (hp)</li>
+                    <li>sirius black (hp)</li>
+                    <li>fred weasley (hp)</li>
+                    <li>george weasley (hp)</li>
+                    <li>augustus waters (tfios)</li>
+                    <li>benjamin blue (v)</li>
+                    <li>hiram stolowitski (v)</li>
+                    </ol>`,
+        content: `<hr>
+            <div>
+                ps: the order of the characters strictly do not matter except for the first two.
+            </div>
+                <hr>
+                blogging date: 8/8/17-11:18pm
+                <hr>
+                update: time finished- 12:22pm.
+                programming is freakin time consuming.
+                <hr>
+                <a href="/">main page</a>
+        </div>`
+    },
+    'blog-two': {},
+    'blog-three': {}
+};
+
+function createtemplate(data)  {
+    var title = data.title;
+    var heading = data.heading;
+    var list = data.list;
+    var content = data.content;
+    
+    var htmltemplate = `<html>
+    <head>
+        <title>
+         ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, intial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+
+    </head>
+    <body bgcolor="yellow">
+        <div class="first">
+            <div>
+                <h1><b>${heading}</b></h1>
+            </div>
+            <div>
+             ${list}
+            ${content}
+        
+    </body>
+</html>`;
+return htmltemplate;
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/blog-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'blog one.html'));
+  res.send(createtemplate(blog-one));
 });
 
 app.get('/blog-two', function (req, res) {
